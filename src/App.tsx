12885dabe1602b241a56708da0a2c5ba713466c8
emsoraffa <emil.rafael.solberg@gmail.com>
2025-03-24 00:37:00 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { ChatBubble } from './components/chatbubble'
+import { ThemeToggle } from './components/themetoggle'
+
+function App() {
+
+  return (
+    <>
+      <ThemeToggle />
+      <ChatBubble />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/chatbubble/chatbubble.tsx
@@ -0,0 +1,9 @@
+
+export const ChatBubble = () => {
+  return (
+    <>
+      <div className="bg-green text-gray-900 rounded-lg ring-shadow-xl dark:bg-green">
+        This will be a chatbubble</div>
+    </>
+  )
+}
