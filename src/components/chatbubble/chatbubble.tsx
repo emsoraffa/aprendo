3b8c4f5c8f6851b54c8f0529a0aac4d99f2adff1
emsoraffa <emil.rafael.solberg@gmail.com>
2025-03-24 22:46:48 +0100

    feat: dark mode setup

--- src/components/chatbubble/chatbubble.tsx
+++ src/components/chatbubble/chatbubble.tsx
@@ -1,9 +1,9 @@
-
 export const ChatBubble = () => {
   return (
     <>
-      <div className="bg-green text-gray-900 rounded-lg ring-shadow-xl dark:bg-green">
-        This will be a chatbubble</div>
+      <div className="bg-white text-gray-900 dark:text-gray-200 dark:bg-green-900 rounded-lg shadow-xl p-4">
+        This will be a chatbubble
+      </div>
     </>
   )
 }
--- src/components/themetoggle/themetoggle.tsx
+++ src/components/themetoggle/themetoggle.tsx
@@ -1,24 +1,61 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const ThemeToggle = () => {
-  const [darkMode, toggleDarkMode] = useState(false);
-  return (
-    <>
-      <div>
+  const [darkMode, setDarkMode] = useState(() => {
+    if (typeof window !== 'undefined') {
+      const savedTheme = localStorage.getItem("theme");
+      return savedTheme === "dark";
+    }
+    return false;
+  });
 
-        <div className="text-gray-700">Darkmode: {darkMode.toString()}</div>
-        <label
-          className="group relative block h-8 w-14 rounded-full bg-gray-300 transition-colors [-webkit-tap-highlight-color:_transparent] has-checked:bg-gray-900 cursor-pointer"
+  useEffect(() => {
+    localStorage.setItem("theme", darkMode ? "dark" : "light");
+
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
+  const toggleTheme = () => setDarkMode(!darkMode);
+
+  return (
+    <div>
+      <div className="text-gray-700">Darkmode: {darkMode.toString()}</div>
+      <label
+        className="group relative block h-8 w-14 rounded-full bg-gray-300 transition-colors [-webkit-tap-highlight-color:_transparent] has-checked:bg-gray-900 cursor-pointer"
+
+      >
+        <input
+          onChange={toggleTheme}
+          checked={darkMode}
+          type="checkbox"
+          id="themeToggle"
+          className="peer sr-only"
+        />
+        <span
+          className="absolute inset-y-0 start-0 m-1 grid size-6 place-content-center rounded-full bg-white text-gray-700 transition-[inset-inline-start] peer-checked:start-6 peer-checked:*:first:hidden *:last:hidden peer-checked:*:last:block"
         >
-          <input onClick={() => { darkMode ? toggleDarkMode(false) : toggleDarkMode(true) }} type="checkbox" id="AcceptConditions" className="peer sr-only" />
-          <span
-            className="absolute inset-y-0 start-0 m-1 grid size-6 place-content-center rounded-full bg-white text-gray-700 transition-[inset-inline-start] peer-checked:start-6 peer-checked:*:first:hidden *:last:hidden peer-checked:*:last:block"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-brightness-down"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /><path d="M12 5l0 .01" /><path d="M17 7l0 .01" /><path d="M19 12l0 .01" /><path d="M17 17l0 .01" /><path d="M12 19l0 .01" /><path d="M7 17l0 .01" /><path d="M5 12l0 .01" /><path d="M7 7l0 .01" /></svg>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-moon"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" /></svg>
-          </span>
-        </label>
-      </div>
-    </>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-brightness-down">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+            <path d="M12 5l0 .01" />
+            <path d="M17 7l0 .01" />
+            <path d="M19 12l0 .01" />
+            <path d="M17 17l0 .01" />
+            <path d="M12 19l0 .01" />
+            <path d="M7 17l0 .01" />
+            <path d="M5 12l0 .01" />
+            <path d="M7 7l0 .01" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-moon">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
+          </svg>
+        </span>
+      </label>
+    </div>
   )
 }
